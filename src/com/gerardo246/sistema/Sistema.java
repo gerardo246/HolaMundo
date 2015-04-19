e59003947234fa0b244607ae938d8ca327c69676
gerardo246 <gerardo_lonely@hotmail.com>
2015-04-19 13:43:33 -0500

    Commit nro. 02 Modificado el mensaje.

--- src/com/gerardo246/sistema/Sistema.java
+++ src/com/gerardo246/sistema/Sistema.java
@@ -2,6 +2,6 @@ package com.gerardo246.sistema;
 
 public class Sistema {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola a Todos!!!");
 	}
 }
